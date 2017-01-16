2c82b948199ba6592a380892334e2a669cccba03
RajeshNPCI <p.rajesh531@gmail.com>
2017-01-17 00:05:48 +0530

    FEWDFS5A1
    FEWDFS5A1

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var	name="Rajesh";
+var	age=26;
+var	dateOfBirth="17Jan1990";
+var	placeOfBirth="kollapur";
+console.log("Name :"+name);
+console.log("Age :"+age);
+console.log("DateOfBirth :"+dateOfBirth);
+console.log("PlaceOfBirth :"+placeOfBirth);
